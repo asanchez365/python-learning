db861668c34ba3e3b73e49087f299e3dabb137c7
Sanchez <asanchez365@cps.k12.il.us>
2024-05-07 09:13:47 -0500

    loops2

---
+++ pythonLearning/day 4/whileReview.py
@@ -0,0 +1,23 @@
+#while loops allow you to repeat code until the condition is met
+#while loops are defined with the while keyboard
+#while loops are used when you dont know how many
+#times you need to repeat the code
+
+# studentList = ["Citlali", "Praise", "Aaron", "Fiona", "Xavier"]
+# StudentName=0
+# while StudentName <len(studentList):
+#     print(studentList[StudentName])
+#     StudentName+=1
+
+# fruitList = ["apple", "banana", "cherry", "date", "elderberry"]
+# #for loop to print the fruitList
+# #for loop is when you know how many times
+# #you need to repeat the code
+# for fruit in fruitList:
+#     print(fruit + "is a fruit.")
+
+studentList = ["Citlali", "Praise", "Aaron", "Fiona", "Xavier"]
+
+for student in studentList:
+    ask= input("What is your last name?")
+    print(student + ""+ ask)
